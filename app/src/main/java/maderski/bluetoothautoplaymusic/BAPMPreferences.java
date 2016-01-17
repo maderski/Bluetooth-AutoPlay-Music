2d9546a05eddd4de7fdd0ed1dc716ffa75753669
Jason Maderski <maderski@gmail.com>
2016-01-16 19:52:17 -0500

    Continued working on Shared Preferences

--- app/src/main/java/maderski/bluetoothautoplaymusic/BAPMPreferences.java
+++ app/src/main/java/maderski/bluetoothautoplaymusic/BAPMPreferences.java
@@ -32,4 +32,49 @@ public class BAPMPreferences {
 
         return context.getSharedPreferences(MY_PREFS_NAME, context.MODE_MULTI_PROCESS);
     }
+
+    public static void setLaunchGoogleMaps(Context context, boolean enabled){
+        editor(context).putBoolean(LAUNCH_MAPS_KEY, enabled);
+    }
+
+    public static boolean getLaunchGoogleMaps(Context context){
+        return reader(context).getBoolean(LAUNCH_MAPS_KEY, false);
+    }
+
+    public static void setKeepScreenON(Context context, boolean enabled){
+        editor(context).putBoolean(KEEP_SCREEN_ON_KEY, enabled);
+    }
+
+    public static boolean getKeepScreenON(Context context) {
+        return reader(context).getBoolean(KEEP_SCREEN_ON_KEY, false);
+    }
+
+    public static void setPriorityMode(Context context, boolean enabled){
+        editor(context).putBoolean(PRIORITY_MODE_KEY, enabled);
+    }
+
+    public static boolean getPriorityMode(Context context){
+        return reader(context).getBoolean(PRIORITY_MODE_KEY, false);
+    }
+
+    public static void setMaxVolume(Context context, boolean enabled){
+        editor(context).putBoolean(MAX_VOLUME_KEY, enabled);
+    }
+
+    public static boolean getMaxVolume(Context context){
+        return reader(context).getBoolean(MAX_VOLUME_KEY, false);
+    }
+
+    public static void setLaunchMusicPlayer(Context context, boolean enabled){
+        editor(context).putBoolean(LAUNCH_MUSIC_PLAYER_KEY, enabled);
+    }
+
+    public static boolean getLaunchMusicPlayer(Context context){
+        return reader(context).getBoolean(LAUNCH_MUSIC_PLAYER_KEY, false);
+    }
+
+    public static void commit(Context context){
+        editor(context).commit();
+        _editor = null;
+    }
 }
