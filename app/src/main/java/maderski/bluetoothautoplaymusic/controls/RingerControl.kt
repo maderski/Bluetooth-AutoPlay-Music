adc4b4549df59b654756b2a0f322f794bcc8f69c
Jason Maderski <maderski@gmail.com>
2018-11-06 20:09:01 -0500

    convert RingerControl to Kotlin

---
+++ app/src/main/java/maderski/bluetoothautoplaymusic/controls/RingerControl.kt
@@ -0,0 +1,77 @@
+package maderski.bluetoothautoplaymusic.controls
+
+import android.Manifest
+import android.app.NotificationManager
+import android.content.Context
+import android.media.AudioManager
+import android.os.Build
+import android.util.Log
+
+import maderski.bluetoothautoplaymusic.helpers.PermissionHelper
+import maderski.bluetoothautoplaymusic.sharedprefs.BAPMPreferences
+
+/**
+ * Created by Jason on 12/8/15.
+ */
+class RingerControl(private val mContext: Context) {
+
+    private val am: AudioManager = mContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+    private val mNotificationManager: NotificationManager = mContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    //turns phone sounds OFF & initialize AudioManager
+    fun soundsOFF() {
+        val isAlreadySilent = am.ringerMode == AudioManager.RINGER_MODE_SILENT
+        if (!isAlreadySilent) {
+            val usePriorityMode = BAPMPreferences.getUsePriorityMode(mContext)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val hasNAPPermission = mNotificationManager.isNotificationPolicyAccessGranted
+                if (usePriorityMode && hasNAPPermission) {
+                    mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_PRIORITY)
+                    Log.d(TAG, "RingerControl: " + "Priority")
+                } else {
+                    putPhoneInSilentMode()
+                }
+            } else {
+                putPhoneInSilentMode()
+            }
+        } else {
+            Log.d(TAG, "Ringer is Already silent")
+        }
+    }
+
+    private fun putPhoneInSilentMode() {
+        am.ringerMode = AudioManager.RINGER_MODE_SILENT
+        Log.d(TAG, "RingerControl: " + "Silent")
+    }
+
+    //turns phone sounds ON
+    fun soundsON() {
+        val usePriorityMode = BAPMPreferences.getUsePriorityMode(mContext)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val permission = Manifest.permission.ACCESS_NOTIFICATION_POLICY
+            val hasNAPPermission = PermissionHelper.isPermissionGranted(mContext, permission)
+            if (usePriorityMode && hasNAPPermission) {
+                mNotificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_ALL)
+                Log.d(TAG, "RingerControl: " + "Normal")
+            } else {
+                am.ringerMode = AudioManager.RINGER_MODE_NORMAL
+                Log.d(TAG, "RingerControl: " + "Normal")
+            }
+        } else {
+            am.ringerMode = AudioManager.RINGER_MODE_NORMAL
+            Log.d(TAG, "RingerControl: " + "Normal")
+        }
+    }
+
+    fun vibrateOnly() {
+        am.ringerMode = AudioManager.RINGER_MODE_VIBRATE
+    }
+
+    fun ringerSetting(): Int {
+        return am.ringerMode
+    }
+
+    companion object {
+        private const val TAG = "RingerControl"
+    }
+}
