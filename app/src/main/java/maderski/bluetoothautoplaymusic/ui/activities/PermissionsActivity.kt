4f6339f417ac5541357de934d8e8c476ec97d087
Jason Maderski <maderski@gmail.com>
2020-06-28 16:31:09 -0400

    continue implementing permissions ui

--- app/src/main/java/maderski/bluetoothautoplaymusic/ui/activities/PermissionsActivity.kt
+++ app/src/main/java/maderski/bluetoothautoplaymusic/ui/activities/PermissionsActivity.kt
@@ -1,7 +1,10 @@
 package maderski.bluetoothautoplaymusic.ui.activities
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_permissions.*
 import maderski.bluetoothautoplaymusic.R
 import maderski.bluetoothautoplaymusic.permission.PermissionManager
 import org.koin.android.ext.android.inject
@@ -12,12 +15,45 @@ class PermissionsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permissions)
 
+        // Course Location
         val isLocationPermissionGranted = permissionManager.isLocationPermissionGranted()
-        val hasUsageStatsPermission = permissionManager.hasUsageStatsPermission()
-        val hasNotificationAccessPermission = permissionManager.hasNotificationAccessPermission()
-        val hasOverlayPermission = permissionManager.hasOverlayPermission()
-        val hasNotificationListenerAccessPermission = permissionManager.hasNotificationListenerAccessPermission()
+        setStatusIcon(iv_location_perm_status, isLocationPermissionGranted)
+        sw_location_perm_toggle.isEnabled = isLocationPermissionGranted
 
+        // Usage Stats
+        val hasUsageStatsPermission = permissionManager.hasUsageStatsPermission()
+        setStatusIcon(iv_usage_stats_perm_status, hasUsageStatsPermission)
+        sw_usage_stats_perm_toggle.isEnabled = hasUsageStatsPermission
+
+        // Notification Access
+        val hasNotificationAccessPermission = permissionManager.hasNotificationAccessPermission()
+        setStatusIcon(iv_notification_access_perm_status, hasNotificationAccessPermission)
+        sw_notification_access_perm_toggle.isEnabled = hasNotificationAccessPermission
+
+        // Overlay
+        val hasOverlayPermission = permissionManager.hasOverlayPermission()
+        setStatusIcon(iv_overlay_perm_status, hasOverlayPermission)
+        sw_overlay_perm_toggle.isEnabled = hasOverlayPermission
+
+        // Notification Listener
+        val hasNotificationListenerAccessPermission = permissionManager.hasNotificationListenerAccessPermission()
+        setStatusIcon(iv_notification_listener_perm_status, hasNotificationListenerAccessPermission)
+        sw_notification_listener_perm_toggle.isEnabled = hasNotificationListenerAccessPermission
+
+        bn_perm_continue.setOnClickListener {
+
+        }
+
+        bn_perm_continue.isEnabled = isLocationPermissionGranted
+                && hasNotificationAccessPermission
+                && hasNotificationListenerAccessPermission
+                && hasOverlayPermission
+                && hasUsageStatsPermission
+    }
+
+    private fun setStatusIcon(imageView: ImageView, isEnabled: Boolean) {
+        val statusIconResId = if (isEnabled) R.drawable.ic_permission_granted else R.drawable.ic_no_permission
+        imageView.setImageDrawable(getDrawable(statusIconResId))
     }
 
 
